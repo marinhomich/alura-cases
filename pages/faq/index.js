ed92080be190df0f721596f367c445ca49fac414
Michel Marinho <michel.marinho04@outlook.com>
2022-12-26 15:43:20 -0400

    Add: Page Title

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {useEffect, useState} from "react";
 import Head from "next/head";
+import PageTitle from "../../src/components/PageTitle";
 
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -26,9 +27,7 @@ export default function FAQPage({faq}) {
 
     return (
         <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
+            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
             <h1>Alura Cases - Página de Perguntas FAQ</h1>
             <Link href='/'>
                 Ir para Home
